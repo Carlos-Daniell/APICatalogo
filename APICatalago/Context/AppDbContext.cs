104d34182f625c6a73f4ed6ffc4135476c11c631
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-02-16 17:19:14 -0300

    Add project files.

---
+++ APICatalago/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using APICatalago.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace APICatalago.Context;
+
+public class AppDbContext : DbContext {
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+
+    }
+
+}
+
